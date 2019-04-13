81278f61ea8b4e26e2bb1d202dfecccb19cabeeb
Lakia Jones <rachandra_jones@yahoo.com>
2019-04-13 11:33:48 -0500

    wip

---
+++ javascripts/helpers/bread.js
@@ -0,0 +1,31 @@
+const bread = [
+    {
+        type: 'Italian',
+        price: 1.50,
+    },
+    {
+        type: 'Honey-Oat',
+        price: 1.50,
+    },
+    {
+        type: 'Rye',
+        price: 1.50,
+    },
+    {
+        type: 'Wheat',
+        price: 1.50,
+    }, {
+        type: 'Sourdough',
+        price: 1.50,
+    },
+    {
+        type: 'Low-Carb Lettuce Bun',
+        price: 2.50,
+    },
+];
+    
+const getBread = () => {
+    return bread;
+};
+
+export default {getBread};
---
+++ javascripts/main.js
@@ -0,0 +1,7 @@
+console.log('Lakia Jones');
+
+const init = () =>{
+    
+};
+
+init();
